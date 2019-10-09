4eb1aa915cc9c4e0f0b4fe8b855500cc4a354fb0
vishalicious213 <vishalicious213@gmail.com>
2019-10-09 17:21:06 -0400

    started coding menu in JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// menu button hover effect
+// menu button image hover effect
 const menuButton = document.querySelector(".menuButton");
 
 menuButton.addEventListener("mouseover", (event) => {
@@ -8,3 +8,35 @@ menuButton.addEventListener("mouseover", (event) => {
 menuButton.addEventListener("mouseout", (event) => {
     menuButton.src = "img/menu_silver.png";
 })
+
+// create menu
+const anchor = document.querySelector("header div");
+
+function createMenu() {
+    const menu = document.createElement("div");
+    const menuItems = document.createElement("ul");
+    const menuItem1 = document.createElement("li"); // home
+    const menuItem2 = document.createElement("li"); // html websites
+    const menuItem3 = document.createElement("li"); // js/dom sites
+
+    menu.appendChild(menuItems);
+    menuItems.appendChild(menuItem1);
+    menuItems.appendChild(menuItem2);
+    menuItems.appendChild(menuItem3);
+
+    menuItem1.textContent = "HOME";
+    menuItem2.textContent = "HTML / CSS WEBSITES";
+    menuItem3.textContent = "JAVASCRIPT & DOM SITES";
+
+    menuItem1.href = "index.html";
+    console.log(menuItem1.href);
+
+    menuButton.addEventListener("click", (event) => {
+
+    })
+
+    anchor.appendChild(menu);
+    return menu;
+}
+
+// createMenu();
